0101f7b77a9a3fa1b671ff3208c8c6ff9bbd40e6
manjuhiremath <manjuhiremath1352@gmail.com>
2025-09-17 17:06:21 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,6 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-
     <html lang="en">
       <head>
         <link rel="icon" href="/Home/icon.jpeg"></link>
